be5481967bf77b1068c797eb750240a06270f79c
Edward Horsey <edwardhorsey@me.com>
2020-02-20 10:06:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,24 +4,16 @@ counter = 0
               if array[counter] == value_to_find
                 return counter 
               end
-      
-      
       counter += 1 
       end
 return nil
-
-
 end
   
-
 def find_max_value(array)
 counter = 0
 highest = 0
      while counter < array.length do
-         
-         
          if array[counter] > highest
-         
          highest = array[counter]
          end
       counter += 1
@@ -35,11 +27,9 @@ def find_min_value(array)
  lowest = array[0]
      while counter < array.length do
          if array[counter] < lowest
-         
          lowest = array[counter]
          end
       counter += 1
      end
 return lowest
 end 
-# end
