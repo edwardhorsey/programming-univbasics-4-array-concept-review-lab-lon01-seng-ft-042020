6740f87fbf1197f452de8a543e1cf5ba471599f7
Edward Horsey <edwardhorsey@me.com>
2020-02-20 09:59:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,12 @@
 def find_element_index(array, value_to_find)
 counter = 0
       while counter < array.length do
-        
+      
+      
+      
+      
+      
+      end
         
         
         
